5b7fd7ab98273746d8b3565ff86346ab5d372488
RichardOkubo <richardlopesneves@gmail.com>
2021-12-13 02:31:27 -0300

    adicionando gradiente

--- src/components/PreviousSong/index.js
+++ src/components/PreviousSong/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { View, Text, FlatList } from "react-native";
+import { LinearGradient } from "expo-linear-gradient";
 
 import AlbumPrevious from "../AlbumPrevious";
 import Options from "../Options";
@@ -9,18 +10,24 @@ import { styles } from "./style";
 
 export default function PreviousSong({ title, albums }) {
   return (
-    <View style={styles.container}>
-      <View style={styles.header}>
-        <Text style={styles.title}>{title}</Text>
-        <Options />
+    <LinearGradient
+      colors={["#1ed860", "#121212"]}
+      start={{ x: -0.2, y: -0.5 }}
+      end={{ x: 0, y: .7 }}
+    >
+      <View style={styles.container}>
+        <View style={styles.header}>
+          <Text style={styles.title}>{title}</Text>
+          <Options />
+        </View>
+        <FlatList
+          data={albums}
+          renderItem={({ item }) => <AlbumPrevious album={item} />}
+          keyExtractor={(item) => item.id}
+          showsHorizontalScrollIndicator={false}
+          horizontal
+        />
       </View>
-      <FlatList
-        data={albums}
-        renderItem={({ item }) => <AlbumPrevious album={item} />}
-        keyExtractor={(item) => item.id}
-        showsHorizontalScrollIndicator={false}
-        horizontal
-      />
-    </View>
+    </LinearGradient>
   );
 }

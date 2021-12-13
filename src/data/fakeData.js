9589e2cb23c75dee8c8abd03a1a53dcb8a0300e9
RichardOkubo <richardlopesneves@gmail.com>
2021-12-13 17:40:12 -0300

    projeto concluido

--- src/data/fakeData.js
+++ src/data/fakeData.js
@@ -18,6 +18,16 @@ export default [
         uri: "https://seed-mix-image.spotifycdn.com/v6/img/artist/2ziB7fzrXBoh1HUPS6sVFn/pt/large",
         bands: "Mix de Audioslave",
       },
+      {
+        id: "4",
+        uri: "https://seed-mix-image.spotifycdn.com/v6/img/artist/4vGrte8FDu062Ntj0RsPiZ/pt/large",
+        bands: "Mix de Polyphia",
+      },
+            {
+        id: "5",
+        uri: "https://seed-mix-image.spotifycdn.com/v6/img/artist/5gs7iemsrjIJbz0ryFcy79/pt/large",
+        bands: "Mix de LVB",
+      },
     ],
   },
   {
@@ -211,27 +221,27 @@ export default [
     title: "Para ficar de boa",
     albums: [
       {
-        id: "31",
+        id: "36",
         uri: "https://i.scdn.co/image/ab67706f00000003fbabce11ab019931ca40c588",
         bands: "Drinks no Sofá",
       },
       {
-        id: "32",
+        id: "37",
         uri: "https://i.scdn.co/image/ab67706f00000003ee42a542a4c259708eff50c1",
         bands: "Resenha na Praia",
       },
       {
-        id: "33",
+        id: "38",
         uri: "https://i.scdn.co/image/ab67706f0000000349085cfc6675ea4050cb56a9",
         bands: "Sol no Domingo",
       },
       {
-        id: "34",
+        id: "39",
         uri: "https://i.scdn.co/image/ab67706f00000003aa12ed2e646eb8a6a160f0d5",
         bands: "Weekend Hangouts",
       },
       {
-        id: "35",
+        id: "40",
         uri: "https://i.scdn.co/image/ab67706f00000003b659dc3b284457a59f190e31",
         bands: "Rocktall Hour",
       },
@@ -242,27 +252,27 @@ export default [
     title: "Relaxante",
     albums: [
       {
-        id: "31",
+        id: "41",
         uri: "https://i.scdn.co/image/ab67706f00000003676811167c28286734f9f698",
         bands: "Stripped Down",
       },
       {
-        id: "32",
+        id: "42",
         uri: "https://i.scdn.co/image/ab67706f00000003cdc960328ce509cfbfd19614",
         bands: "Modern Chili Rock",
       },
       {
-        id: "33",
+        id: "43",
         uri: "https://i.scdn.co/image/ab67706f0000000374ea894d6bcef53f524aeab0",
         bands: "Cafezinho",
       },
       {
-        id: "34",
+        id: "44",
         uri: "https://i.scdn.co/image/ab67706f00000003cc12e168aa503c85e0266c3d",
         bands: "Rock Acústico",
       },
       {
-        id: "35",
+        id: "45",
         uri: "https://i.scdn.co/image/ab67706f0000000312cf5fd4624cce89f748ec99",
         bands: "Peaceful Meditation",
       },
@@ -273,27 +283,27 @@ export default [
     title: "Para treinar",
     albums: [
       {
-        id: "31",
+        id: "46",
         uri: "https://i.scdn.co/image/ab67706f000000032b6a4f0a0f453f9b52601dde",
         bands: "Running to Rock 170-190 BPM",
       },
       {
-        id: "32",
+        id: "47",
         uri: "https://i.scdn.co/image/ab67706f00000003929e39d3550da276515b31f2",
         bands: "Adrenaline Workout",
       },
       {
-        id: "33",
+        id: "48",
         uri: "https://i.scdn.co/image/ab67706f00000003b6ce3ad849b97fb35ee91cb2",
         bands: "Rock Me UP!",
       },
       {
-        id: "34",
+        id: "49",
         uri: "https://i.scdn.co/image/ab67706f0000000382894e9aedb86fd9f2f724db",
         bands: "One More Rep",
       },
       {
-        id: "35",
+        id: "50",
         uri: "https://i.scdn.co/image/ab67706f0000000318fd64de72fa5ae23e261e5d",
         bands: "Rock 'n' Run 150-180 BPM",
       },
@@ -304,27 +314,27 @@ export default [
     title: "Viagem",
     albums: [
       {
-        id: "31",
+        id: "51",
         uri: "https://i.scdn.co/image/ab67706f000000036931836ba4d6cb4c2b923060",
         bands: "Retrôvisor",
       },
       {
-        id: "32",
+        id: "52",
         uri: "https://i.scdn.co/image/ab67706f00000003dd548e58b230bd41973378e9",
         bands: "Viagem Sertaneja",
       },
       {
-        id: "33",
+        id: "53",
         uri: "https://i.scdn.co/image/ab67706f00000003fd076b5e9bdffcd1929c7290",
         bands: "Classic Road Trip Songs",
       },
       {
-        id: "34",
+        id: "54",
         uri: "https://i.scdn.co/image/ab67706f00000003e3c90ac71e1315a0561d5c13",
         bands: "Rádio Bora",
       },
       {
-        id: "35",
+        id: "55",
         uri: "https://i.scdn.co/image/ab67706f000000031b84822d2ae33438903454df",
         bands: "70s Road Trip",
       },
@@ -335,27 +345,27 @@ export default [
     title: "Seu astral",
     albums: [
       {
-        id: "31",
+        id: "56",
         uri: "https://i.scdn.co/image/ab67706f000000036f39cc1479a845dcc9e7c54b",
         bands: "Metal Ballads",
       },
       {
-        id: "32",
+        id: "57",
         uri: "https://i.scdn.co/image/ab67706f0000000368eb4355c65b3b65db6f9581",
         bands: "Walk Like A Badass",
       },
       {
-        id: "33",
+        id: "58",
         uri: "https://i.scdn.co/image/ab67706f000000030c527c5b3a3398fc24082d5b",
         bands: "Trabalhando de Casa",
       },
       {
-        id: "34",
+        id: "59",
         uri: "https://i.scdn.co/image/ab67706f00000003cf795aff62b2cd850dd58190",
         bands: "Deep Dark Indie",
       },
       {
-        id: "35",
+        id: "60",
         uri: "https://i.scdn.co/image/ab67706f00000003676811167c28286734f9f698",
         bands: "Stripped Down",
       },
@@ -366,29 +376,29 @@ export default [
     title: "Artistas sugeridos",
     artists: [
       {
-        id: "31",
-        uri: "https://community.spotify.com/t5/image/serverpage/image-id/55829iC2AD64ADB887E2A5/image-size/large?v=v2&px=999",
-        name: "?",
+        id: "61",
+        uri: "https://i.scdn.co/image/ab67706c0000bebb132592c603bf37999f614f07",
+        name: "Eminem",
       },
       {
-        id: "32",
-        uri: "https://community.spotify.com/t5/image/serverpage/image-id/55829iC2AD64ADB887E2A5/image-size/large?v=v2&px=999",
-        name: "?",
+        id: "62",
+        uri: "https://i.scdn.co/image/ab67616d00001e02a1181cfa90ffb86ff3415947",
+        name: "Haikaiss",
       },
       {
-        id: "33",
-        uri: "https://community.spotify.com/t5/image/serverpage/image-id/55829iC2AD64ADB887E2A5/image-size/large?v=v2&px=999",
-        name: "?",
+        id: "63",
+        uri: "https://i.scdn.co/image/ab67616d00001e0257fc4730e06c9ab20c1e073b",
+        name: "Skrillex",
       },
       {
-        id: "34",
-        uri: "https://community.spotify.com/t5/image/serverpage/image-id/55829iC2AD64ADB887E2A5/image-size/large?v=v2&px=999",
-        name: "?",
+        id: "64",
+        uri: "https://i.scdn.co/image/ab67616d00001e020e39e0b5b70d4890aa5e8096",
+        name: "Charlie Brown Jr.",
       },
       {
-        id: "35",
-        uri: "https://community.spotify.com/t5/image/serverpage/image-id/55829iC2AD64ADB887E2A5/image-size/large?v=v2&px=999",
-        name: "?",
+        id: "65",
+        uri: "https://i.scdn.co/image/ab67616d00001e02c6710b4a52539506c30e5354",
+        name: "Black Eyed Peas",
       },
     ],
   },
@@ -397,27 +407,27 @@ export default [
     title: "Músicas novinhas em folha",
     albums: [
       {
-        id: "31",
+        id: "66",
         uri: "https://i.scdn.co/image/ab67706f00000003e974b6f99efdfbef5a6f69f5",
         bands: "Novidades da Semana",
       },
       {
-        id: "32",
+        id: "67",
         uri: "https://i.scdn.co/image/ab67706f000000035411bf46aa5e3609622c3eb6",
         bands: "Bonde do Funk",
       },
       {
-        id: "33",
+        id: "68",
         uri: "https://i.scdn.co/image/ab67706f00000003779095e116530cda31f90b9f",
         bands: "New Core",
       },
       {
-        id: "34",
+        id: "69",
         uri: "https://i.scdn.co/image/ab67706f00000003f894a8abe1ab948678d8bb15",
         bands: "All New Rock",
       },
       {
-        id: "35",
+        id: "70",
         uri: "https://i.scdn.co/image/ab67706f00000003521b9e5a9b901442cac4d183",
         bands: "Novos Beats",
       },
@@ -428,27 +438,27 @@ export default [
     title: "Instrumental",
     albums: [
       {
-        id: "31",
+        id: "71",
         uri: "https://i.scdn.co/image/ab67706f00000003b70e0223f544b1faa2e95ed0",
         bands: "Sleep",
       },
       {
-        id: "32",
+        id: "72",
         uri: "https://i.scdn.co/image/ab67706f0000000312cf5fd4624cce89f748ec99",
         bands: "Peaceful Meditation",
       },
       {
-        id: "33",
+        id: "73",
         uri: "https://i.scdn.co/image/ab67706f00000003c1ebf493192d994ccd2bed87",
         bands: "Concentração Perfeita",
       },
       {
-        id: "34",
+        id: "74",
         uri: "https://i.scdn.co/image/ab67706f000000031f011a86f8135dcec991be26",
         bands: "Relaxing Message",
       },
       {
-        id: "35",
+        id: "75",
         uri: "https://i.scdn.co/image/ab67706f0000000319239a7beb890662e4b59cc6",
         bands: "Deep Listening",
       },
@@ -459,27 +469,27 @@ export default [
     title: "Recomendado para hoje",
     albums: [
       {
-        id: "31",
+        id: "76",
         uri: "https://i.scdn.co/image/ab67616d00001e022d73b494efcb99356f8c7b28",
         bands: "Mer De Noms",
       },
       {
-        id: "32",
+        id: "77",
         uri: "https://i.scdn.co/image/ab67616d00001e02777f349834457f80787871d2",
         bands: "Black Gives Way To Blue",
       },
       {
-        id: "33",
+        id: "78",
         uri: "https://i.scdn.co/image/ab67616d00001e02e7d4bfff8e7e87c6090591b6",
         bands: "Contraband",
       },
       {
-        id: "34",
+        id: "79",
         uri: "https://i.scdn.co/image/ab67616d00001e02b6b955741846e463b2f96caa",
         bands: "No. 4",
       },
       {
-        id: "35",
+        id: "80",
         uri: "https://i.scdn.co/image/ab67616d00001e02528903c46e4426b95a2c6d36",
         bands: "Short Bus",
       },
@@ -490,27 +500,27 @@ export default [
     title: "Episódios para você",
     albums: [
       {
-        id: "31",
+        id: "81",
         uri: "https://i.scdn.co/image/ab67656300005f1f4d9a6c61c089e353f24ee566",
         bands: "E Se?",
       },
       {
-        id: "32",
+        id: "82",
         uri: "https://i.scdn.co/image/ab67656300005f1f1e65e0d111d78201ae7e23c3",
         bands: "Morte e loucura",
       },
       {
-        id: "33",
+        id: "83",
         uri: "https://i.scdn.co/image/ab67656300005f1f2f097933309fa576b2af1f30",
         bands: "Podpah",
       },
       {
-        id: "34",
+        id: "84",
         uri: "https://i.scdn.co/image/58e4e737473db80f4fee1454b42f4d753c47e286",
         bands: "Flow Podcast",
       },
       {
-        id: "35",
+        id: "85",
         uri: "https://i.scdn.co/image/ab67656300005f1f11dce77c1ddae7e9295f90d9",
         bands: "Mano a Mano",
       },
@@ -521,27 +531,27 @@ export default [
     title: "Podcasts originais e exclusivos do Spotify",
     albums: [
       {
-        id: "31",
+        id: "86",
         uri: "https://i.scdn.co/image/ab67656300005f1f7cb1f18282ae4b8931269ec3",
         bands: "Horóscopo Hoje",
       },
       {
-        id: "32",
+        id: "87",
         uri: "https://i.scdn.co/image/ab67656300005f1fa665c5b78791d5d671b55de8",
         bands: "123 Segundos",
       },
       {
-        id: "33",
+        id: "88",
         uri: "https://i.scdn.co/image/75b406b088a7b94ae12fc60f7631f8f7a462fdab",
         bands: "Matando Robôs Gigantes",
       },
       {
-        id: "34",
+        id: "89",
         uri: "https://i.scdn.co/image/ab67656300005f1fc3d58d8b52489af2fb461a6a",
         bands: "Respondendo em Voz Alta",
       },
       {
-        id: "35",
+        id: "90",
         uri: "https://i.scdn.co/image/ab67656300005f1fb94035a6b6650d0781a90a46",
         bands: "Calcinha Larga",
       },
@@ -552,27 +562,27 @@ export default [
     title: "Programas para conhecer",
     albums: [
       {
-        id: "31",
+        id: "91",
         uri: "https://i.scdn.co/image/ab67656300005f1f4eba7a3880a42144ae0a9662",
         bands: "PeeWeeCast",
       },
       {
-        id: "32",
+        id: "92",
         uri: "https://i.scdn.co/image/ab67656300005f1fee8fb4751a225d2d5bdef922",
         bands: "Primocast",
       },
       {
-        id: "33",
+        id: "93",
         uri: "https://i.scdn.co/image/ab67656300005f1fb02b40e89324364b8b5f75b2",
         bands: "Mano a Mano",
       },
       {
-        id: "34",
+        id: "94",
         uri: "https://i.scdn.co/image/ab67656300005f1f7eab5a6a23a1595edd5616d4",
         bands: "NerdCast",
       },
       {
-        id: "35",
+        id: "95",
         uri: "https://i.scdn.co/image/ab67656300005f1fab9e1a6aa19acf018c865849",
         bands: "Diário de um Jovem Moderno",
       },
